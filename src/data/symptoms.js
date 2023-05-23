450f97cc56995764b8ace31e579d88819ce86378
Aryan23132 <aryanj21@iiserb.ac.in>
2023-05-24 02:29:46 +0530

    Symptoms done

---
+++ src/data/symptoms.js
@@ -0,0 +1,20 @@
+import { collection, getDocs , query, query, where} from "firebase/firestore";
+import { firestore } from "./firebase";
+
+// Get all the questions
+export const getAllSymptoms = (problem) => {
+  // Get a reference to the `questions` collection
+  const colRef = collection(firestore, "symptoms");
+
+  // Run the query to fetch symptoms of particular problem
+  const q = query(colRef,where("problem","==",problem)) 
+  //const query = getDocs(colRef);
+  //   query.then((snapshot) =>
+  //     snapshot.docs.forEach((doc) => {
+  //       console.log(doc.data());
+  //     })
+  //   );
+
+  // Return the query
+  return q;
+};
