13e0569899652324cabf5e755bfc3e58b13c485f
Sattwik Sahu <sattwik21@iiserb.ac.in>
2023-05-24 02:55:32 +0530

    Added testing code for queries

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import { getAllSymptoms } from "./data/symptoms.js";
+import { onSnapshot } from "firebase/firestore";
+
+const query = getAllSymptoms("DEP");
+onSnapshot(query, (snapshot) => {
+  snapshot.docs.forEach((doc) => {
+    console.log({ id: doc.id, ...doc.data() });
+  });
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
