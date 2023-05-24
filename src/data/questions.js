238e7ab72accb0d56de6872e07cca894b916dc79
Siddhi Lipare <siddhi21@iiserb.ac.in>
2023-05-24 05:35:19 +0530

    test

--- src/data/questions.js
+++ src/data/questions.js
@@ -11,33 +11,7 @@ export const getAllQuestions = () => {
   const q_ANX = query(colRef,where("problem","==","ANX"));
   const q_SLP = query(colRef,where("problem","==","SLP"));
   const q_STR = query(colRef,where("problem","==","STR"));
-//   function flattenQuery() {
-//     const query = [];
-//   // Create an empty array
-// //const queries = [];
-// // Add queries to the array using push()
-//  queries.push(q_DEP);
-//  queries.push(q_ANX);
-//  queries.push(q_STR);
-//  queries.push(q_SLP);
-//  const flattenedQuery = [].concat(...query);
-
-//  const flattenedArray = flattenQuery();
-
-//  console.log(flattenedArray);
-// }
+return q_ANX;
 };
-// Alternatively, you can directly assign the queries to the array
-// const queries = ["Query 1", "Query 2", "Query 3", "Query 4"
 
-
-
-  //const query = getDocs(colRef);
-  //   query.then((snapshot) =>
-  //     snapshot.docs.forEach((doc) => {
-  //       console.log(doc.data());
-  //     })
-  //   );
-
-  // Return the query
 
